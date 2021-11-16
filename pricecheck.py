dcf349852f581df84b268a87a5839b62842ab8a7
eugene3711 <57406011+eugene3711@users.noreply.github.com>
2021-11-16 16:56:22 +0500

    Create pricecheck.py

---
+++ pricecheck.py
@@ -0,0 +1,13 @@
+import requests
+from bs4 import BeautifulSoup
+
+testurl = 'https://www.andrewgeoghegan.com/product/cannele-twist-diamond/'
+
+def get_status(url):
+    page = requests.get(url)
+    soup = BeautifulSoup(page.content, 'html.parser')
+    price = soup.find('span', class_='woocommerce-Price-amount')
+    price = int(price.get_text()[1:-3].replace(',',''))
+    return price
+
+print(get_status(testurl))
