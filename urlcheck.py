41d6a95efb2a0b648cad6cac670bf41a80be7a52
eugene3711 <57406011+eugene3711@users.noreply.github.com>
2022-02-02 14:06:36 +0500

    Rental status

--- urlcheck.py
+++ urlcheck.py
@@ -11,6 +11,20 @@ testurl = 'https://www.mccarthyandstone.co.uk/retirement-properties-for-sale/bal
 def get_status(url):
     page = requests.get(url)
     soup = BeautifulSoup(page.content, 'html.parser')
-    stage = soup.find('div', class_='cta-block-header__group')
+    try:
+        stage = soup.find('div', class_='cta-block-header__group').get_text()
+    except:
+        
+        try:
+            stage = soup.find('div', class_ = 'cta-block-stage__footer').get_text()
+            
+        except:
+            stage = 'n/a'
 
-    return stage.get_text()
+    return stage
+
+
+camp = 'https://www.mccarthyandstone.co.uk/retirement-properties-for-sale/campbell-house-weybridge/'
+
+print(get_status(testurl))
+print(get_status(camp))
